665bed700086e7ebeb4c4f33e10fd27b5bec755f
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-08-20 12:54:24 +0300

    add basic kana cases

--- kanaToRomaji_test.go
+++ kanaToRomaji_test.go
@@ -6,10 +6,145 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestVowels(t *testing.T) {
-	const expectedRes = "aiueo"
+func TestBasic(t *testing.T) {
+	const expectedRes = "aiueon"
 
-	for _, v := range [2]string{"あいうえお", "アイウエオ"} {
+	for _, v := range [2]string{"あいうえおん", "アイウエオン"} {
+		res, _ := KanaToRomaji(v)
+		assert.Equal(t, expectedRes, res)
+	}
+}
+
+func TestBasicDakuten(t *testing.T) {
+	const expectedRes = "vu"
+
+	for _, v := range [2]string{"ゔ", "ヴ"} {
+		res, _ := KanaToRomaji(v)
+		assert.Equal(t, expectedRes, res)
+	}
+}
+
+func TestK(t *testing.T) {
+	const expectedRes = "kakikukeko"
+
+	for _, v := range [2]string{"かきくけこ", "カキクケコ"} {
+		res, _ := KanaToRomaji(v)
+		assert.Equal(t, expectedRes, res)
+	}
+}
+
+func TestKDakuten(t *testing.T) {
+	const expectedRes = "gagigugego"
+
+	for _, v := range [2]string{"がぎぐげご", "ガギグゲゴ"} {
+		res, _ := KanaToRomaji(v)
+		assert.Equal(t, expectedRes, res)
+	}
+}
+
+func TestS(t *testing.T) {
+	const expectedRes = "sashisuseso"
+
+	for _, v := range [2]string{"さしすせそ", "サシスセソ"} {
+		res, _ := KanaToRomaji(v)
+		assert.Equal(t, expectedRes, res)
+	}
+}
+
+func TestSDakuten(t *testing.T) {
+	const expectedRes = "zajizuzezo"
+
+	for _, v := range [2]string{"ざじずぜぞ", "ザジズゼゾ"} {
+		res, _ := KanaToRomaji(v)
+		assert.Equal(t, expectedRes, res)
+	}
+}
+
+func TestT(t *testing.T) {
+	const expectedRes = "tachitsuteto"
+
+	for _, v := range [2]string{"たちつてと", "タチツテト"} {
+		res, _ := KanaToRomaji(v)
+		assert.Equal(t, expectedRes, res)
+	}
+}
+
+func TestTDakuten(t *testing.T) {
+	const expectedRes = "dajizudedo"
+
+	for _, v := range [2]string{"だじづでど", "ダジヅデド"} {
+		res, _ := KanaToRomaji(v)
+		assert.Equal(t, expectedRes, res)
+	}
+}
+
+func TestN(t *testing.T) {
+	const expectedRes = "naninuneno"
+
+	for _, v := range [2]string{"なにぬねの", "ナニヌネノ"} {
+		res, _ := KanaToRomaji(v)
+		assert.Equal(t, expectedRes, res)
+	}
+}
+
+func TestH(t *testing.T) {
+	const expectedRes = "hahifuheho"
+
+	for _, v := range [2]string{"はひふへほ", "ハヒフヘホ"} {
+		res, _ := KanaToRomaji(v)
+		assert.Equal(t, expectedRes, res)
+	}
+}
+
+func TestHDakutenB(t *testing.T) {
+	const expectedRes = "babibubebo"
+
+	for _, v := range [2]string{"ばびぶべぼ", "バビブベボ"} {
+		res, _ := KanaToRomaji(v)
+		assert.Equal(t, expectedRes, res)
+	}
+}
+
+func TestHDakutenP(t *testing.T) {
+	const expectedRes = "papipupepo"
+
+	for _, v := range [2]string{"ぱぴぷぺぽ", "パピプペポ"} {
+		res, _ := KanaToRomaji(v)
+		assert.Equal(t, expectedRes, res)
+	}
+}
+
+func TestM(t *testing.T) {
+	const expectedRes = "mamimumemo"
+
+	for _, v := range [2]string{"まみむめも", "マミムメモ"} {
+		res, _ := KanaToRomaji(v)
+		assert.Equal(t, expectedRes, res)
+	}
+}
+
+func TestY(t *testing.T) {
+	const expectedRes = "yayuyo"
+
+	for _, v := range [2]string{"やゆよ", "ヤユヨ"} {
+		res, _ := KanaToRomaji(v)
+		assert.Equal(t, expectedRes, res)
+	}
+}
+
+func TestR(t *testing.T) {
+	const expectedRes = "rarirurero"
+
+	for _, v := range [2]string{"らりるれろ", "ラリルレロ"} {
+		res, _ := KanaToRomaji(v)
+		assert.Equal(t, expectedRes, res)
+	}
+}
+
+func TestW(t *testing.T) {
+	const expectedRes = "wawiwewo"
+
+	for _, v := range [2]string{"わゐゑを", "ワヰヱヲ"} {
 		res, _ := KanaToRomaji(v)
 		assert.Equal(t, expectedRes, res)
 	}
--- src.go
+++ src.go
@@ -17,25 +17,259 @@ func KanaToRomaji(str string) (string, error) {
 		return "", errors.New("all characters must be kana (3-bit unicode characters)")
 	}
 
-	var bld strings.Builder
-	bld.Grow(len(str) * 2)
+	var sb strings.Builder
+	sb.Grow(len(str) * 2)
 
 	for i := 0; i < len(str); i += byteCount {
+		var romaji string
+
 		switch getKanaRune(str[i], str[i+1], str[i+2]) {
+		// basic
 		case 'あ', 'ア':
-			bld.WriteByte('a')
+			sb.WriteByte('a')
+			continue
 		case 'い', 'イ':
-			bld.WriteByte('i')
+			sb.WriteByte('i')
+			continue
 		case 'う', 'ウ':
-			bld.WriteByte('u')
+			sb.WriteByte('u')
+			continue
 		case 'え', 'エ':
-			bld.WriteByte('e')
+			sb.WriteByte('e')
+			continue
 		case 'お', 'オ':
-			bld.WriteByte('o')
+			sb.WriteByte('o')
+			continue
+		case 'ん', 'ン':
+			romaji = "n"
+			goto Romaji
+		// basic dakuten
+		case 'ゔ', 'ヴ':
+			romaji = "vu"
+			goto Romaji
+		// k
+		case 'か', 'カ':
+			romaji = "ka"
+			goto Romaji
+		case 'き', 'キ':
+			romaji = "ki"
+			goto Romaji
+		case 'く', 'ク':
+			romaji = "ku"
+			goto Romaji
+		case 'け', 'ケ':
+			romaji = "ke"
+			goto Romaji
+		case 'こ', 'コ':
+			romaji = "ko"
+			goto Romaji
+		// k dakuten (g)
+		case 'が', 'ガ':
+			romaji = "ga"
+			goto Romaji
+		case 'ぎ', 'ギ':
+			romaji = "gi"
+			goto Romaji
+		case 'ぐ', 'グ':
+			romaji = "gu"
+			goto Romaji
+		case 'げ', 'ゲ':
+			romaji = "ge"
+			goto Romaji
+		case 'ご', 'ゴ':
+			romaji = "go"
+			goto Romaji
+		// s
+		case 'さ', 'サ':
+			romaji = "sa"
+			goto Romaji
+		case 'し', 'シ':
+			romaji = "shi"
+			goto Romaji
+		case 'す', 'ス':
+			romaji = "su"
+			goto Romaji
+		case 'せ', 'セ':
+			romaji = "se"
+			goto Romaji
+		case 'そ', 'ソ':
+			romaji = "so"
+			goto Romaji
+		// s dakuten (z)
+		case 'ざ', 'ザ':
+			romaji = "za"
+			goto Romaji
+		case 'じ', 'ジ':
+			romaji = "ji"
+			goto Romaji
+		case 'ず', 'ズ':
+			romaji = "zu"
+			goto Romaji
+		case 'ぜ', 'ゼ':
+			romaji = "ze"
+			goto Romaji
+		case 'ぞ', 'ゾ':
+			romaji = "zo"
+			goto Romaji
+		// t
+		case 'た', 'タ':
+			romaji = "ta"
+			goto Romaji
+		case 'ち', 'チ':
+			romaji = "chi"
+			goto Romaji
+		case 'つ', 'ツ':
+			romaji = "tsu"
+			goto Romaji
+		case 'て', 'テ':
+			romaji = "te"
+			goto Romaji
+		case 'と', 'ト':
+			romaji = "to"
+			goto Romaji
+		// t dakuten (d)
+		case 'だ', 'ダ':
+			romaji = "da"
+			goto Romaji
+		case 'ぢ', 'ヂ':
+			romaji = "ji"
+			goto Romaji
+		case 'づ', 'ヅ':
+			romaji = "zu"
+			goto Romaji
+		case 'で', 'デ':
+			romaji = "de"
+			goto Romaji
+		case 'ど', 'ド':
+			romaji = "do"
+			goto Romaji
+		// n
+		case 'な', 'ナ':
+			romaji = "na"
+			goto Romaji
+		case 'に', 'ニ':
+			romaji = "ni"
+			goto Romaji
+		case 'ぬ', 'ヌ':
+			romaji = "nu"
+			goto Romaji
+		case 'ね', 'ネ':
+			romaji = "ne"
+			goto Romaji
+		case 'の', 'ノ':
+			romaji = "no"
+			goto Romaji
+		// h
+		case 'は', 'ハ':
+			romaji = "ha"
+			goto Romaji
+		case 'ひ', 'ヒ':
+			romaji = "hi"
+			goto Romaji
+		case 'ふ', 'フ':
+			romaji = "fu"
+			goto Romaji
+		case 'へ', 'ヘ':
+			romaji = "he"
+			goto Romaji
+		case 'ほ', 'ホ':
+			romaji = "ho"
+			goto Romaji
+		// h dakuten (b)
+		case 'ば', 'バ':
+			romaji = "ba"
+			goto Romaji
+		case 'び', 'ビ':
+			romaji = "bi"
+			goto Romaji
+		case 'ぶ', 'ブ':
+			romaji = "bu"
+			goto Romaji
+		case 'べ', 'ベ':
+			romaji = "be"
+			goto Romaji
+		case 'ぼ', 'ボ':
+			romaji = "bo"
+			goto Romaji
+		// h dakuten (p)
+		case 'ぱ', 'パ':
+			romaji = "pa"
+			goto Romaji
+		case 'ぴ', 'ピ':
+			romaji = "pi"
+			goto Romaji
+		case 'ぷ', 'プ':
+			romaji = "pu"
+			goto Romaji
+		case 'ぺ', 'ペ':
+			romaji = "pe"
+			goto Romaji
+		case 'ぽ', 'ポ':
+			romaji = "po"
+			goto Romaji
+		// m
+		case 'ま', 'マ':
+			romaji = "ma"
+			goto Romaji
+		case 'み', 'ミ':
+			romaji = "mi"
+			goto Romaji
+		case 'む', 'ム':
+			romaji = "mu"
+			goto Romaji
+		case 'め', 'メ':
+			romaji = "me"
+			goto Romaji
+		case 'も', 'モ':
+			romaji = "mo"
+			goto Romaji
+		// y
+		case 'や', 'ヤ':
+			romaji = "ya"
+			goto Romaji
+		case 'ゆ', 'ユ':
+			romaji = "yu"
+			goto Romaji
+		case 'よ', 'ヨ':
+			romaji = "yo"
+			goto Romaji
+		// r
+		case 'ら', 'ラ':
+			romaji = "ra"
+			goto Romaji
+		case 'り', 'リ':
+			romaji = "ri"
+			goto Romaji
+		case 'る', 'ル':
+			romaji = "ru"
+			goto Romaji
+		case 'れ', 'レ':
+			romaji = "re"
+			goto Romaji
+		case 'ろ', 'ロ':
+			romaji = "ro"
+			goto Romaji
+		// w
+		case 'わ', 'ワ':
+			romaji = "wa"
+			goto Romaji
+		case 'ゐ', 'ヰ':
+			romaji = "wi"
+			goto Romaji
+		case 'ゑ', 'ヱ':
+			romaji = "we"
+			goto Romaji
+		case 'を', 'ヲ':
+			romaji = "wo"
+			goto Romaji
 		}
+
+	Romaji:
+		sb.WriteString(romaji)
+		continue
 	}
 
-	return bld.String(), nil
+	return sb.String(), nil
 }
 
 //	getKanaRune converts a 3-bit hex value to its unicode code point
